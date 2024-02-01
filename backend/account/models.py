056e9d8655e24e1671ffe6b619914fe6e12f3a9d
Roshis <roshis.tamang@gmail.com>
2024-02-01 12:26:29 +1100

    Register UserRegister model in admin

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class UserRegister(models.Model):
+        MALE = 'M'
+        FEMALE = 'F'
+        OTHER = 'O'
+
+        GENDER_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHER, 'Other'),
+    ]
+        username = models.CharField(max_length=30)
+        email=models.EmailField(max_length=50)
+        dateofbirth = models.DateTimeField()
+        gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+        password = models.PasswordField()
+        confirm_password = models.PasswordField()
+        created_at = models.DateTimeField(auto_now_add=True)
+        
+        def __init__(self):
+            self.username
+            

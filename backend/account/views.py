d5d3a8dbd9aa4ee19265330ab976ad96d98f0ff4
Ronish Lopxhan <rlopxhan21@gmail.com>
2024-02-03 02:49:44 +1100

    add linting pipeline and add ipdb for debugger

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,26 +1,22 @@
 from django.contrib.auth.models import User
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import generics, serializers, status
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .serializers import UserRegisterSerializer
+from .serializers import RegisterSerializer
+
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserRegisterSerializer
+    serializer_class = RegisterSerializer
 
     def create(self, request, *args, **kwargs):
-        serializer = UserRegisterSerializer(data=request.data)
+        serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.create(serializer.validated_data)
-            tokens = self.get_tokens_for_user(user)
-            return Response(tokens, status=status.HTTP_201_CREATED)
+            refresh_token = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh_token),
+                'access': str(refresh_token.access_token),
+                }, status=status.HTTP_201_CREATED)
+
         raise serializers.ValidationError({ 'errors': serializer.errors}, code=status.HTTP_400_BAD_REQUEST)
-
-    def get_tokens_for_user(self,user):
-        refresh = RefreshToken.for_user(user)
-
-        return {
-        'refresh': str(refresh),
-        'access': str(refresh.access_token),
-    }

bed1c499fd5ab4b517487fc87241474a1d12f092
rlopxhan21 <110797314+rlopxhan21@users.noreply.github.com>
2024-02-06 23:38:39 -0800

    Add basic login  (#8)
    * add login and some improvement in register code
    
    * logout created with JWT
    
    ---------
    
    Co-authored-by: Roshis <roshis.tamang@gmail.com>

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path, include
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenBlacklistView
 from .views import RegisterView
+
+
 urlpatterns = [
-    path('register/', RegisterView.as_view(),name="reegister-user"),
-]
+    path('register/', RegisterView.as_view(),name="register_user"),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('refresh-token/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
+            ]

7f8d38539ef1c334eb04d43d7577e19be3ac9ea9
Roshis <roshis.tamang@gmail.com>
2024-02-14 11:02:07 +1100

    Initialize the user model

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model): 
+   #getting first name and last name and email from the register model & and nickname as username as well but if only true
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDER_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        ]
+    
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    address=models.CharField()
+    job=models.CharField(max_length=20)
+    sports=models.CharField(max_length=20, blank=True)
+    hobby=models.CharField(max_length=20, blank=True)
+    job=models.CharField(max_length=20, blank=True)
+    mobile_number=models.CharField(max_length=20, blank=True)
+    education=models.CharField(max_length=20, blank=True)
+    country=models.CharField(max_length=20, blank=True)
+    # photo featured
+    # connections
+    # friends/ following
+    
+    
+
+    
+    
+    
+    
+    

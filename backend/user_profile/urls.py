db2d88de38f36783c439dcf4b7ec8c39765369c6
Roshis <roshis.tamang@gmail.com>
2024-02-27 16:14:06 +1100

    fixing urls errors

--- backend/user_profile/urls.py
+++ backend/user_profile/urls.py
@@ -3,5 +3,5 @@ from .views import UserProfileList, UserProfileDetail
 
 urlpatterns = [
     path('profiles/', UserProfileList.as_view(), name='userprofile-list'),
-    path('profiles/detail/', UserProfileDetail.as_view(), name='userprofile-detail'),
+    path('profiles_detail/<str:user__username>/', UserProfileDetail.as_view(), name='userprofile-detail'),
 ]
--- backend/user_profile/views.py
+++ backend/user_profile/views.py
@@ -6,17 +6,19 @@ from .serializers import UserProfileSerializer
 class UserProfileList(generics.ListCreateAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
-    permission_classes = [IsAuthenticated]  # Ensure user is authenticated
+    permission_classes = [IsAuthenticated] 
+    
+    def get_queryset(self):
+        return UserProfile.objects.filter(user=self.request.user)# Ensure user is authenticated
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)  # Set the user of UserProfile to the currently logged-in user
 
 class UserProfileDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated] 
-    queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
-    
+    permission_classes = [IsAuthenticated]
+    lookup_field = 'user__username'
+
     def get_queryset(self):
-        user = self.request.user
-        return UserProfile.objects.filter(user=user)
-  
+        return UserProfile.objects.filter(user=self.request.user)
+    
